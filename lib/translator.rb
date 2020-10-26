dca2a72bd3819e41355a2ba88a4e2634e9d854d4
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-26 23:42:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def load_library(file)
   new_emoticons
 end
 
-def get_japanese_emoticon(file, symbol)
+def get_japanese_emoticon(file, emoticon)
   # code goes here
   load_library(file)
   #binding.pry
