f3ca4c967cae737a89bde68331b78b1d802ecc56
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-26 21:12:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 # require modules here
-
+require 'yaml'
+require "pry"
 def load_library
   # code goes here
+  emoticons = YAML.load_file('emoticons.yml')
+  binding.pry 
+
+fruits
 end
 
 def get_japanese_emoticon
